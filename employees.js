27e3b630801f0e6638c48f457a39479fa172d74c
Zack White <zhwhite18@yahoo.com>
2020-09-29 16:02:55 -0500

    created db & tables

--- employees.js
+++ employees.js
@@ -1,3 +1,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+function start() {
+    inquirer
+  .prompt({
+      type: "list",
+      name: "selection",
+      message: "What would you like to do?",
+      choices: ["View All Employees", "View Departments", "View Roles", "Add Employee", "Add Department", "Add Role", "Update An Employee"]
+  })
+  
+}
---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE employees_db;
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Legal"), ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000.00, 1), ("Lead Engineer", 150000.00, 2), ("Legal Team Lead", 250000.00, 3), ("Accountant", 125000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Zack", "White", 1, 1), ("Tom", "Brady", 2, NULL), ("Phil", "Loy", 3, NULL), ("John", "Wick", 4, 1);
