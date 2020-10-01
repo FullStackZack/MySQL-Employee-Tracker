a0acd0b591903e710b7c369c21aa5d5a4635c040
Zack White <zhwhite18@yahoo.com>
2020-10-01 11:53:44 -0500

    updated tables & completed addEmployee function

--- employees.js
+++ employees.js
@@ -23,7 +23,7 @@ function start() {
       type: "list",
       name: "selection",
       message: "What would you like to do?",
-      choices: ["View All Employees", "View Departments", "View Roles", "Add Employee", "Add Department", "Add Role", "Update An Employee"]
+      choices: ["View All Employees", "View Departments", "View Roles", "Add Employee", "Add Department", "Add Role", "Update An Employee", "Quit"]
   })
   .then(function(answer) {
       switch(answer.selection) {
@@ -47,6 +47,9 @@ function start() {
               break;
           case "Update An Employee":
               updateEmployee();
+              break;
+          default:
+              connection.end();
               break;
       }
   })
@@ -88,4 +91,69 @@ function viewRoles() {
     });
 };
 
+function showAll() {
+    var query = "SELECT * FROM employee, role, department";
 
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+
+        console.table(res);
+    })
+}
+
+function addEmployee() {
+
+    var query = "SELECT * FROM employee, role";
+
+    connection.query(query, function(err, res) {
+        inquirer
+        .prompt([
+            {
+                name: "first_name",
+                type: "input",
+                message: "What is the employee's first name?"
+            },
+            {
+                name: "last_name",
+                type: "input",
+                message: "What is the employee's last name?"
+            },
+            {
+                name: "role_choice",
+                type: "rawlist",
+                message: "What will be their role?",
+                choices: function() {
+                    let choiceArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        choiceArr.push(res[i].id);
+                    }
+                    return choiceArr;
+                }
+            },
+            {
+                name: "manager_choice",
+                type: "rawlist",
+                message: "Who will be their manager?",
+                choices: function() {
+                    let choiceArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        choiceArr.push(res[i].manager_id);
+                    }
+                    return choiceArr;
+                }
+            }
+        ])
+        .then(function(answer) {
+            
+            connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)"
+            , [answer.first_name, answer.last_name, answer.role_choice, answer.manager_choice], function(err, data) {
+                if(err) throw err;
+
+                console.table("Employee successfully added!");
+                showAll();
+
+                start();
+            })
+        })
+    })   
+}
--- schema.sql
+++ schema.sql
@@ -5,24 +5,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    PRIMARY KEY (id)
 );
